0a4d320972df65c0791e985dd6d6087e28dd4c71
John Lawson <john@jwlawson.co.uk>
2012-11-03 17:31:50 +0000

    Initial commit
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Hyperbolic/src/uk/co/jwlawson/hyperbolic/client/Hyperbolic.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2012 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.jwlawson.hyperbolic.client;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.canvas.dom.client.CssColor;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Touch;
+import com.google.gwt.event.dom.client.GestureStartEvent;
+import com.google.gwt.event.dom.client.GestureStartHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchEndHandler;
+import com.google.gwt.event.dom.client.TouchMoveEvent;
+import com.google.gwt.event.dom.client.TouchMoveHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * @author John Lawson
+ */
+public class Hyperbolic implements EntryPoint {
+
+	static final String holderId = "canvasholder";
+
+	static final String upgradeMessage = "Your browser does not support the HTML5 Canvas. Please upgrade your browser to view this demo.";
+
+	Canvas canvas;
+	Canvas backBuffer;
+
+	// mouse positions relative to canvas
+	int mouseX, mouseY;
+
+	// timer refresh rate, in milliseconds
+	static final int refreshRate = 25;
+
+	// canvas size, in px
+	static final int height = 400;
+	static final int width = 400;
+
+	final CssColor redrawColor = CssColor.make("rgba(255,255,255,0.6)");
+	Context2d context;
+	Context2d backBufferContext;
+
+	@Override
+	public void onModuleLoad() {
+		canvas = Canvas.createIfSupported();
+		backBuffer = Canvas.createIfSupported();
+		if (canvas == null) {
+			RootPanel.get(holderId).add(new Label(upgradeMessage));
+			return;
+		}
+
+		// init the canvases
+		canvas.setWidth(width + "px");
+		canvas.setHeight(height + "px");
+		canvas.setCoordinateSpaceWidth(width);
+		canvas.setCoordinateSpaceHeight(height);
+		backBuffer.setCoordinateSpaceWidth(width);
+		backBuffer.setCoordinateSpaceHeight(height);
+		RootPanel.get(holderId).add(canvas);
+		context = canvas.getContext2d();
+		backBufferContext = backBuffer.getContext2d();
+
+		// init handlers
+		initHandlers();
+
+		// setup timer
+		final Timer timer = new Timer() {
+			@Override
+			public void run() {
+				doUpdate();
+			}
+		};
+		timer.scheduleRepeating(refreshRate);
+	}
+
+	void doUpdate() {
+		// update the back canvas
+		backBufferContext.setFillStyle(redrawColor);
+		backBufferContext.fillRect(0, 0, width, height);
+	}
+
+	void initHandlers() {
+		canvas.addMouseMoveHandler(new MouseMoveHandler() {
+			@Override
+			public void onMouseMove(MouseMoveEvent event) {
+				mouseX = event.getRelativeX(canvas.getElement());
+				mouseY = event.getRelativeY(canvas.getElement());
+			}
+		});
+
+		canvas.addMouseOutHandler(new MouseOutHandler() {
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				mouseX = -200;
+				mouseY = -200;
+			}
+		});
+
+		canvas.addTouchMoveHandler(new TouchMoveHandler() {
+			@Override
+			public void onTouchMove(TouchMoveEvent event) {
+				event.preventDefault();
+				if (event.getTouches().length() > 0) {
+					Touch touch = event.getTouches().get(0);
+					mouseX = touch.getRelativeX(canvas.getElement());
+					mouseY = touch.getRelativeY(canvas.getElement());
+				}
+				event.preventDefault();
+			}
+		});
+
+		canvas.addTouchEndHandler(new TouchEndHandler() {
+			@Override
+			public void onTouchEnd(TouchEndEvent event) {
+				event.preventDefault();
+				mouseX = -200;
+				mouseY = -200;
+			}
+		});
+
+		canvas.addGestureStartHandler(new GestureStartHandler() {
+			@Override
+			public void onGestureStart(GestureStartEvent event) {
+				event.preventDefault();
+			}
+		});
+	}
+
+}
---
+++ Hyperbolic/src/uk/co/jwlawson/hyperbolic/client/ui/Options.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2012 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.jwlawson.hyperbolic.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author John
+ * 
+ */
+public class Options extends Composite {
+
+	private static OptionsUiBinder uiBinder = GWT.create(OptionsUiBinder.class);
+
+	interface OptionsUiBinder extends UiBinder<Widget, Options> {
+	}
+
+	public Options() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@UiField
+	Button button;
+
+	public Options(String firstName) {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		// Can access @UiField after calling createAndBindUi
+		button.setText(firstName);
+	}
+
+	@UiHandler("button")
+	void onClick(ClickEvent e) {
+		Window.alert("Hello!");
+	}
+
+}
